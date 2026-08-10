9a75b7af95dd9b4293125eab92dc9eca0d5e76a1
Caseywebs <kibetpalvin91@gmail.com>
2026-08-10 22:52:51 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const qrRoute = require('./qr');
 const pairRoute = require('./pair');
+const sessionRoute = require('./session');
 
 module.exports = {
     qrRoute,
-    pairRoute
+    pairRoute,
+    sessionRoute
 }
